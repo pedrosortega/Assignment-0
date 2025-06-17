db5ab96774ba0ae772187133cc1a97745da28f4e
pedrosortega <psortega2004@gmail.com>
2025-06-16 22:51:34 -0400

    test-07 completed

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,6 +1,15 @@
 function countOfAllIndexMatchingNumbers(nums) {
-  // Insert code here;
+  let count = 0;
+
+  for(let i = 0; i < nums.length;i++){
+    if(nums[i]===i){
+      count++;
+    }
+  }
+  return count;
 }
+
+countOfAllIndexMatchingNumbers([0,1,2,3]);
 
 // Do not edit this line;
 module.exports = countOfAllIndexMatchingNumbers;
